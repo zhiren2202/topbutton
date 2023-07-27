08ca04b0b2c3f47ef9e7c12b13f9ce61457f0468
zhiren2202 <132483183+zhiren2202@users.noreply.github.com>
2023-07-27 17:42:14 +0900

    Add files via upload

--- resources/js/top_button.js
+++ resources/js/top_button.js
@@ -1,35 +1,21 @@
+
+const topBtn = $("[class*='top_btn']");
+
+
 // TOP2. 캐릭터버튼 (스크롤 내리면 점프)
 $(window).scroll(function() {
-  var yOffset = window.scrollY || document.documentElement.scrollTop;
-  var topBtn2 = $(".top_button .top_btn2");
-
-  if (yOffset > 0) {
-      topBtn2.addClass("jump");
-  } else {
-      topBtn2.removeClass("jump");
-  }
-
-});
-
-// TOP4. 스크롤 내리면 보이는 버튼
-$(window).scroll(function () {
-  var yOffset = window.scrollY || document.documentElement.scrollTop;
-  var topBtn4 = $(".top_button .top_btn4");
-  if (yOffset > 0) {
-    topBtn4.removeClass("top");
-  } else {
-    topBtn4.addClass("top");
-  }
+  const yOffset = window.scrollY || document.documentElement.scrollTop;
+  yOffset > 0 ? topBtn.addClass("scroll") : topBtn.removeClass("scroll");
 });
 
 
 // TOP5. 스크롤 내리면 보이고 올리면 안 보이는 버튼 (fade in/out)
-$(function () {
-  let lastScrollY = 0;
-  addEventListener("scroll", function () {
-    const scrollY = window.scrollY;
-    const direction = scrollY > lastScrollY ? $(".top_btn5").fadeIn() : $(".top_btn5").fadeOut();
-    lastScrollY = scrollY;
-    console.log(direction);
-  });
-});
+// $(function () {
+//   let lastScrollY = 0;
+//   addEventListener("scroll", function () {
+//     const scrollY = window.scrollY;
+//     const direction = scrollY > lastScrollY ? $(".top_btn5").fadeIn() : $(".top_btn5").fadeOut();
+//     lastScrollY = scrollY;
+//     console.log(direction);
+//   });
+// });
